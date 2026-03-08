01924938d33d389b6f61964a76c6cb649b4ef059
Rahul Gupta <rahulgupta79777@gmail.com>
2026-03-08 15:30:17 +0530

    Time: 0 ms (100%), Space: 12.8 MB (55.15%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -18,11 +18,20 @@ public:
         return false;
     }
     string findDifferentBinaryString(vector<string>& nums) {
-       unordered_set<string> set;
-       for(auto it:nums) set.insert(it);
-       string ans="";
-       string p="";
-       ok(p,ans,set);
-       return ans; 
+    //    unordered_set<string> set;
+    //    for(auto it:nums) set.insert(it);
+    //    string ans="";
+    //    string p="";
+    //    ok(p,ans,set);
+    //    return ans; 
+
+
+    //another using diagonal thing diff from each string from atleast one position
+    string ans="";
+    for(int i=0;i<nums.size();i++){
+        if(nums[i][i]=='0') ans+='1';
+        else ans+='0';
+    }
+    return ans;
     }
 };
