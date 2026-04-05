e5ce289e3626c38650eb86ad76492aeeafa35adc
Rahul Gupta <rahulgupta79777@gmail.com>
2026-04-05 23:15:27 +0530

    Time: 4 ms (83.74%), Space: 19.5 MB (85.03%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        string a="";
-        string b="";
-        string c="";
-        string d="";
+        // string a="";
+        // string b="";
+        // string c="";
+        // string d="";
+        // for(int i=0;i<s1.length();i++){
+        //     if(i%2==0){
+        //         a+=s1[i];
+        //         b+=s2[i];
+        //     }else{
+        //         c+=s1[i];
+        //         d+=s2[i];
+        //     }
+        // }
+        // sort(a.begin(),a.end());
+        // sort(b.begin(),b.end());
+        // sort(c.begin(),c.end());
+        // sort(d.begin(),d.end());
+        // return a==b&&c==d;
+
+        //a little more optimal 
+        
+        int arr1[26];
+        int arr2[26];
         for(int i=0;i<s1.length();i++){
             if(i%2==0){
-                a+=s1[i];
-                b+=s2[i];
+                arr1[s1[i]-'a']++;
+                arr1[s2[i]-'a']--;
             }else{
-                c+=s1[i];
-                d+=s2[i];
+                arr2[s1[i]-'a']++;
+                arr2[s2[i]-'a']--;
             }
         }
-        sort(a.begin(),a.end());
-        sort(b.begin(),b.end());
-        sort(c.begin(),c.end());
-        sort(d.begin(),d.end());
-        return a==b&&c==d;
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=0||arr2[i]!=0) return false;
+        }
+        return true;
     }
 };
