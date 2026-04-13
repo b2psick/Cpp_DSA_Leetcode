044e6fcdeb36d41752a06c3da3bb3dc34e2ff7f6
Rahul Gupta <rahulgupta79777@gmail.com>
2026-04-13 21:08:12 +0530

    Time: 18 ms (33.9%), Space: 88.8 MB (31.91%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        stack<int> st;
+        vector<int> ok(heights.size());
+        for(int i=heights.size()-1;i>=0;i--){
+            bool flag=false;
+            while(st.size()>0){
+                if(heights[i]==18) cout<<ok[i]<<endl;
+                if(!flag){
+                    if(heights[st.top()]>heights[i]){
+                        ok[i]++;
+                        break;
+                    }else{
+                        ok[i]=st.top()-i;
+                        flag=true;
+                        st.pop();
+                    }
+                }else{
+                    if(heights[st.top()]>heights[i]){
+                        ok[i]++;
+                        break;
+                    }
+                    else{
+                        ok[i]++;
+                        st.pop();
+                    }
+                }
+            }
+            st.push(i);
+        }
+        return ok;
+    }
+};
