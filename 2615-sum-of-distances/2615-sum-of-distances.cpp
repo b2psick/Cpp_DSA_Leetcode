599a9fc92d94b912d3cb4550a6bbea5d5aa498d1
Rahul Gupta <rahulgupta79777@gmail.com>
2026-04-23 14:44:42 +0530

    Time: 129 ms (12.5%), Space: 133.2 MB (12%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        unordered_map<int,long long> mp;
+        unordered_map<int,long long> mp1;
+        vector<long long> ans;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]+=i;
+            mp1[nums[i]]++;
+        }
+        unordered_map<int,long long> mp2;
+        unordered_map<int,long long> mp3;
+        for(int i=0;i<nums.size();i++){
+            long long sum=0;
+            sum+=abs(mp2[nums[i]]-i*mp3[nums[i]]);
+            mp2[nums[i]]+=i;
+            mp3[nums[i]]++;
+            sum+=abs((mp[nums[i]]-mp2[nums[i]])-i*(mp1[nums[i]]-mp3[nums[i]]));
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
