ecc60b1add387a0752af083d38b2cb5e7af8ced3
Rahul Gupta <rahulgupta79777@gmail.com>
2026-04-24 11:42:19 +0530

    Time: 127 ms (13.2%), Space: 133.3 MB (12.18%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -10,9 +10,9 @@ public:
         }
         unordered_map<int,long long> mp2;
         unordered_map<int,long long> mp3;
+        //here is also a thing that why we are calculating the left right side seperately because the index smaller than current would give something in abs but if we combine left and right they would not because it would be right-left and thats why we calculate both the left and right seperately but if there was no abs use then we would have combined them
         for(int i=0;i<nums.size();i++){
             long long sum=0;
-            //here is also a thing that why we are calculating the left right side seperately because the index smaller than current would give something in abs but if we combine left and right they would not because it would be right-left and thats why we calculate both the left and right seperately but if there was no abs use then we would have combine them
             sum+=abs(mp2[nums[i]]-i*mp3[nums[i]]);
             mp2[nums[i]]+=i;
             mp3[nums[i]]++;
