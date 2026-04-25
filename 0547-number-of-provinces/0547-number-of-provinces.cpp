a6665381d77ed04cdfdab15a0610f95702234c3c
Rahul Gupta <rahulgupta79777@gmail.com>
2026-04-26 01:15:41 +0530

    Time: 1 ms (47.25%), Space: 19.5 MB (73.09%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,64 @@
+class DisjointSet{
+public:
+    vector<int> rank,parent,size;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    int findUPar(int node){
+        if(node==parent[node])
+            return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        DisjointSet ds(isConnected.size()-1);
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected.size();j++){
+                if(isConnected[i][j]==1){
+                    ds.unionBySize(i,j);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<ds.parent.size();i++){
+            if(ds.parent[i]==i) ans++;
+        }
+        return ans;
+    }
+};
