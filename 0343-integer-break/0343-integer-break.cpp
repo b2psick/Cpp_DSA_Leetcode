3f107d5327b1777d1ece0fa68d6f40a66e728d41
Rahul Gupta <rahulgupta79777@gmail.com>
2026-05-01 02:08:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (43.24%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
+    // int solve(int n){
+    //     if(n <= 3) return n;
+    // }
     int integerBreak(int n) {
-        int ans=1;
+        // int ans=1;
+        // if(n==2) return 1;
+        // if(n==3) return 2;
+        // while(n!=0){
+        //     if(n==6){
+        //         ans=ans*9;
+        //         break;
+        //     }else if(n>=5){
+        //         ans=ans*3;
+        //         n-=3;
+        //     }else{
+        //         ans=ans*2;
+        //         n-=2;
+        //     }
+        // }
+        // return ans;
+        if(n==1) return 1;
         if(n==2) return 1;
         if(n==3) return 2;
-        while(n!=0){
-            if(n==6){
-                ans=ans*9;
-                break;
-            }else if(n>=5){
-                ans=ans*3;
-                n-=3;
-            }else{
-                ans=ans*2;
-                n-=2;
-            }
+        int ans=1;
+        if(n%3==0){
+            ans=ans*pow(3,n/3);
+        }else if(n%3==1){
+            ans=ans*pow(3,n/3-1)*4;
+        }else{
+            ans=ans*pow(3,n/3)*2;
         }
         return ans;
     }
