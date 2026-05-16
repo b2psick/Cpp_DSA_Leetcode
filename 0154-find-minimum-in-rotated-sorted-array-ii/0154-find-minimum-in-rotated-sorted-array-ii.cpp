ce724ff5d2afe82f303ba47f56a9d6d1e4f0d873
Rahul Gupta <rahulgupta79777@gmail.com>
2026-05-17 01:42:50 +0530

    Time: 0 ms (100%), Space: 16.3 MB (27.29%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -6,18 +6,12 @@ public:
         int min1=INT_MAX;
         while(st<=ed){
             int mid=st+(ed-st)/2;
-            if(nums[mid]<min1){
-                min1=nums[mid];
-            }
-            while(st<ed&&nums[st]==nums[ed]){
-                if(nums[st]<min1) min1=nums[st];
-                st++;
-                ed--;
-            }
             if(nums[mid]>nums[ed]){
                 st=mid+1;
-            }else ed=mid-1;
+            }else if(nums[mid]<nums[ed]){
+                ed=mid;
+            }else ed--;
         }
-        return min1;
+        return nums[st];
     }
 };
