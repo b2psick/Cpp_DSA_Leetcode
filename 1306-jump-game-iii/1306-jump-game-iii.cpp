14b82d7d03cc32b66e0311bbc5b15f66451fe229
Rahul Gupta <rahulgupta79777@gmail.com>
2026-05-17 22:53:11 +0530

    Time: 0 ms (100%), Space: 41 MB (18.77%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool ok(vector<int>& arr,int i,vector<int>& vis){
+        if(arr[i]==0) return true;
+        if(vis[i]) return false;
+        vis[i]=true;
+        if(i+arr[i]<arr.size()){
+            if(ok(arr,i+arr[i],vis)) return true;
+        }
+        if(i-arr[i]>=0){
+            if(ok(arr,i-arr[i],vis)) return true;
+        }
+        vis[i]=false;
+        return false;
+    }
+    bool canReach(vector<int>& arr, int start) {
+        vector<int> vis(arr.size());
+        return ok(arr,start,vis);
+    }
+};
