22d6f69904eaead724f9acefe469a4e084cf1596
Rahul Gupta <rahulgupta79777@gmail.com>
2026-06-23 03:08:40 +0530

    Time: 86 ms (5.04%), Space: 84.4 MB (17.58%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -5,24 +5,16 @@ public:
         for(auto it:costs) max1=max(max1,it);
         vector<int> ok(max1+1);
         for(auto it:costs) ok[it]++;
-        for(int i=1;i<ok.size();i++){
-            ok[i]=ok[i]+ok[i-1];
-        }
-        vector<int> sorted;
         int a=0;
-        for(int i=0;i<ok.size();i++){
-            for(int j=0;j<ok[i]-a;j++){
-                sorted.push_back(i);
-            }
-            a=ok[i];
-        }
         int ans=0;
-        for(auto it:sorted){
-            if(coins<it){
+        for(int i=1;i<ok.size();i++){
+            if((long long)i*(long long)ok[i]>coins){
+                if(coins==0) return ans;
+                ans+=coins/i;
                 return ans;
             }
-            coins-=it;
-            ans++;
+            ans+=ok[i];
+            coins-=i*ok[i];
         }
         return ans;
     }
