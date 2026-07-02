3a305b6e1a947d76f2ff9e5ea04f6e1e84155362
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-02 12:22:52 +0530

    Time: 13 ms (76.58%), Space: 30.1 MB (95.69%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        priority_queue<pair<int,int>> pq;
+        if(grid[0][0]==1) health--;
+        vector<vector<int>> ok(grid.size(),vector<int>(grid[0].size(),-1));
+        vector<pair<int,int>> dir;
+        dir.push_back({1,0});
+        dir.push_back({-1,0});
+        dir.push_back({0,1});
+        dir.push_back({0,-1});
+        pq.push({health,0});
+        while(!pq.empty()){
+            if(pq.top().first<1) return false;
+            int i=pq.top().second/grid[0].size();
+            int j=pq.top().second%grid[0].size();
+            int a=pq.top().first;
+            pq.pop();
+            if(i==grid.size()-1&&j==grid[0].size()-1) return true;
+            for(auto it:dir){
+                int ni=i+it.first;
+                int nj=j+it.second;
+                if(ni<0||nj<0||ni>=grid.size()||nj>=grid[0].size()) continue;
+                if(grid[ni][nj]==1){
+                    if(ok[ni][nj]<a-1){
+                        pq.push({a-1,ni*grid[0].size()+nj});
+                        ok[ni][nj]=a-1;
+                    }
+                }else{
+                    if(ok[ni][nj]<a){
+                        pq.push({a,ni*grid[0].size()+nj});
+                        ok[ni][nj]=a;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
