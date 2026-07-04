d60b0bfc36031d95a98d26d537e86dc3a57666ee
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-04 12:44:36 +0530

    Time: 410 ms (37.4%), Space: 287.9 MB (34.45%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
-        priority_queue<pair<int,int>> pq;
+        queue<int> q;
         vector<vector<int>> ok(n,vector<int>(m));
         for(auto it:sources){
             ok[it[0]][it[1]]=it[2];
-            pq.push({it[2],it[0]*m+it[1]});
+            q.push(it[0]*m+it[1]);
         }
         vector<pair<int,int>> dir;
         dir.push_back({0,1});
         dir.push_back({1,0});
         dir.push_back({-1,0});
         dir.push_back({0,-1});
-        while(!pq.empty()){
-            int size=pq.size();
-            queue<pair<int,int>> q;
+        while(!q.empty()){
+            int size=q.size();
+            unordered_set<int> set;
             for(int k=0;k<size;k++){
-                int i=pq.top().second/m;
-                int j=pq.top().second%m;
-                int c=pq.top().first;
-                pq.pop();
+                int i=q.front()/m;
+                int j=q.front()%m;
+                int c=ok[i][j];
+                q.pop();
                 for(auto it:dir){
                     int ni=i+it.first;
                     int nj=j+it.second;
                     if(ni<0||nj<0||ni>=n||nj>=m) continue;
                     if(ok[ni][nj]==0){
                         ok[ni][nj]=c;
-                        q.push({c,ni*m+nj});
+                        set.insert(ni*m+nj);
+                        q.push(ni*m+nj);
+                    }else{
+                        if(ok[ni][nj]>c) continue;
+                        else if(set.find(ni*m+nj)!=set.end()){
+                            ok[ni][nj]=c;
+                        }
                     }
                 }
-            }
-            while(!q.empty()){
-                pq.push(q.front());
-                q.pop();
             }
         }
         return ok;
