163c15bece2b36ecd7206f4bc1a96fb58108a5d9
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-04 11:05:57 +0530

    Time: 0 ms (100%), Space: 9.4 MB (53.36%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    //memo
+    // int ok(int m1,int n1,vector<vector<int>>& dp){
+    //     if(m1==dp.size()-1&&n1==dp[0].size()-1) return 1;
+    //     if(m1>=dp.size()||n1>=dp[0].size()) return 0;
+    //     if(dp[m1][n1]!=-1) return dp[m1][n1];
+    //     int a=ok(m1+1,n1,dp);
+    //     int b=ok(m1,n1+1,dp);
+    //     dp[m1][n1]=a+b;
+    //     return a+b;
+    // }
+    int uniquePaths(int m, int n) {
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        // return ok(0,0,dp);
+
+        //tabulation
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
