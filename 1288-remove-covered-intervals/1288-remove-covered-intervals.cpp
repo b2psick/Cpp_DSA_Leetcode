60b695cc9436156d4e46e43ad5aae7d4297a368d
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-06 14:42:45 +0530

    Time: 9 ms (14.17%), Space: 15.9 MB (12.58%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -7,15 +7,25 @@ public:
             }
             return a[0]<b[0];
         });
-        vector<vector<int>> ok;
-        ok.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(ok.back()[0]<=intervals[i][0]&&ok.back()[1]>=intervals[i][1]){
-                continue;
-            }else{
-                ok.push_back(intervals[i]);
+        // vector<vector<int>> ok;
+        // ok.push_back(intervals[0]);
+        // for(int i=1;i<intervals.size();i++){
+        //     if(ok.back()[0]<=intervals[i][0]&&ok.back()[1]>=intervals[i][1]){
+        //         continue;
+        //     }else{
+        //         ok.push_back(intervals[i]);
+        //     }
+        // }
+
+        //the prev one also works but the space is unnecessary in the prev one
+        int max1=0;
+        int ans=0;
+        for(auto it:intervals){
+            if(it[1]>max1){
+                max1=it[1];
+                ans++;
             }
         }
-        return ok.size();
+        return ans;
     }
 };
