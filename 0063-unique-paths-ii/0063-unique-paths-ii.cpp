a98c645db15542af4380ed2436cb17dab71f0312
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-14 23:12:13 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.63%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-    int ok(vector<vector<int>>& grid,vector<vector<int>>& dp,int i,int j){
-        if(grid[i][j]==1) return 0;
-        if(i==grid.size()-1&&j==grid[0].size()-1){
-            return 1;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int sum=0;
-        if(i<grid.size()-1){
-            sum+=ok(grid,dp,i+1,j);
-        }
-        if(j<grid[0].size()-1){
-            sum+=ok(grid,dp,i,j+1);
-        }
-        dp[i][j]=sum;
-        return sum;
-    }
+
+    // memoization
+    // int ok(vector<vector<int>>& grid,vector<vector<int>>& dp,int i,int j){
+    //     if(grid[i][j]==1) return 0;
+    //     if(i==grid.size()-1&&j==grid[0].size()-1){
+    //         return 1;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int sum=0;
+    //     if(i<grid.size()-1){
+    //         sum+=ok(grid,dp,i+1,j);
+    //     }
+    //     if(j<grid[0].size()-1){
+    //         sum+=ok(grid,dp,i,j+1);
+    //     }
+    //     dp[i][j]=sum;
+    //     return sum;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return ok(grid,dp,0,0);
+        // vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+        // return ok(grid,dp,0,0);
+
+        vector<int> dp(grid[0].size());
+        dp[0]=1;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                int p=0;
+                if(j>0){
+                    p=dp[j-1];
+                }
+                if(grid[i][j]!=1){
+                    dp[j]+=p;
+                }else{
+                    dp[j]=0;
+                }
+            }
+        }
+        return dp[dp.size()-1];
     }
 };
