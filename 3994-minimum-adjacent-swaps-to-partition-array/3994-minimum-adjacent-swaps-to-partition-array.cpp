871397d484cc04658242ee9b5701e5c99719149b
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-22 12:06:10 +0530

    Time: 1903 ms (5.03%), Space: 561.6 MB (5.03%) - LeetHub

---
+++ 3994-minimum-adjacent-swaps-to-partition-array/3994-minimum-adjacent-swaps-to-partition-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long merge(vector<int>& arr, int left, int mid, int right, vector<int> aux) {
+        int i = left;
+        int j = mid + 1;
+        long long count = 0;
+        while(i <= mid || j <= right) {
+            if(i >= mid + 1) aux.push_back(arr[j++]);
+            else if(j >= right+1) aux.push_back(arr[i++]);
+            else if(arr[i] <= arr[j]) aux.push_back(arr[i++]);
+            else {
+                aux.push_back(arr[j++]);
+                count += mid - i + 1;
+            }
+        }
+        for(int i = left; i <= right; i++) {
+            arr[i] = aux[i - left];
+        }
+        return count;
+    }
+    long long mergeSort(vector<int>& arr, int left, int right, vector<int> aux) {
+        if(left >= right) return 0;
+        int mid = left + (right - left) / 2;
+        long long count = 0;
+        
+        count += mergeSort(arr, left, mid, aux)%1000000007;
+        count += mergeSort(arr, mid + 1, right, aux)%1000000007;
+        count += merge(arr, left, mid, right, aux)%1000000007;
+        
+        return count%1000000007;
+    }
+    int minAdjacentSwaps(vector<int>& nums, int a, int b) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<a) nums[i]=0;
+            else if(nums[i]>b) nums[i]=2;
+            else nums[i]=1;
+        }
+        vector<int> aux;
+        long long ans=mergeSort(nums,0,nums.size()-1,aux)%1000000007;
+        return ans;
+    }
+};
