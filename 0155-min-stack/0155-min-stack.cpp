46d5357a58096835c30f5f18073bc9a9fc509983
Rahul Gupta <rahulgupta79777@gmail.com>
2026-07-28 00:10:32 +0530

    Time: 57 ms (54.39%), Space: 151.1 MB (55.01%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,36 @@
 class MinStack {
 public:
-    stack<int> st1;
-    stack<int> st;
+    // stack<int> st1;
+    // stack<int> st;
+
+    //another solution by using one stack of pair in which we calculate min as we push everytime also we can do this using linkelist too
+    stack<pair<int,int>> st;
     MinStack() {
         
     }
     
     void push(int value) {
-        st.push(value);
-        if(st1.empty() || st1.top() >= value){
-            st1.push(value);
-        }
+        // st.push(value);
+        // if(st1.empty() || st1.top() >= value){
+        //     st1.push(value);
+        // }
+        if(st.empty()){
+            st.push({value,value});
+        }else st.push({value,min(value,st.top().second)});
     }
     
     void pop() {
-        if(st1.top() == st.top()) st1.pop();
+        // if(st1.top() == st.top()) st1.pop();
+        // st.pop();
         st.pop();
     }
     
     int top() {
-        return st.top();
+        return st.top().first;
     }
     
     int getMin() {
-        return st1.top();
+        return st.top().second;
     }
 };
 
