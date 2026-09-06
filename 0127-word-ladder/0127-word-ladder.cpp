fd79d24114749718ffa0973eb4349784aa43a943
Rahul Gupta <rahulgupta79777@gmail.com>
2026-09-07 02:28:52 +0530

    Time: 106 ms (33.3%), Space: 21.3 MB (58.6%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,32 +2,10 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        unordered_map<string, vector<string>> mp;
         unordered_set<string> set;
+        unordered_set<string> words;
 
-        for(int i = 0; i < wordList.size(); i++) {
-            for(int j = i + 1; j < wordList.size(); j++) {
-                int diff = 0;
-                for(int k = 0; k < beginWord.length(); k++) {
-                    if(wordList[i][k] != wordList[j][k]) {
-                        diff++;
-                    }
-                    if(diff > 1) break;
-                }
-                if(diff == 1) {
-                    mp[wordList[i]].push_back(wordList[j]);
-                    mp[wordList[j]].push_back(wordList[i]);
-                }
-            }
-            int beginDiff = 0;
-            for(int j = 0; j < beginWord.length(); j++) {
-                if(wordList[i][j] != beginWord[j]) beginDiff++;
-            }
-            if(beginDiff == 1) {
-                mp[beginWord].push_back(wordList[i]);
-                mp[wordList[i]].push_back(beginWord);
-            }
-        }
+        for(auto it : wordList) words.insert(it);
 
         queue<string> q;
 
@@ -42,11 +20,17 @@ public:
             for(int i = 0; i < size; i++) {
                 string front = q.front();
                 q.pop();
-                for(auto it : mp[front]) {
-                    if(it == endWord) return ans;
-                    if(set.find(it) == set.end()) {
-                        set.insert(it);
-                        q.push(it);
+                for(int j = 0; j < beginWord.size(); j++) {
+                    for(int k = 0; k < 26; k++) {
+                        string a = front;
+                        a[j] = 'a' + k;
+                        if(words.find(a) != words.end()) {
+                            if(a == endWord) return ans;
+                            if(set.find(a) == set.end()) {
+                                set.insert(a);
+                                q.push(a);
+                            }
+                        }
                     }
                 }
             }
