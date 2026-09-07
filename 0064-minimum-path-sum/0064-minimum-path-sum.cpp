cfd3d80d0a6aae812cfc8803e8e630dbb0d22440
Rahul Gupta <rahulgupta79777@gmail.com>
2026-09-07 23:28:12 +0530

    Time: 0 ms (100%), Space: 15.5 MB (79.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,19 +23,26 @@ public:
         // return ok(grid,0,0,dp);
 
         //tabulation
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        dp[0][0]=grid[0][0];
-        for(int i=1;i<grid.size();i++){
-            dp[i][0]=grid[i][0]+dp[i-1][0];
-        }
-        for(int i=1;i<grid[0].size();i++){
-            dp[0][i]=grid[0][i]+dp[0][i-1];
-        }
-        for(int i=1;i<grid.size();i++){
-            for(int j=1;j<grid[0].size();j++){
-                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+        
+        vector<int> dp(grid[0].size());
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(i==0 && j==0) {
+                    dp[j] = grid[0][0];
+                    continue;
+                }
+                else if(i==0) {
+                    dp[j] = grid[i][j] + dp[j-1];
+                    continue; 
+                }    
+            
+                if(j == 0) {
+                    dp[j] += grid[i][0];
+                }else{
+                    dp[j] = grid[i][j] + min(dp[j-1], dp[j]);
+                }
             }
         }
-        return dp[grid.size()-1][grid[0].size()-1];
+        return dp[dp.size()-1];
     }
 };
