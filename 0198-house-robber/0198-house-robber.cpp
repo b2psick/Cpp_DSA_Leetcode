34a3255b5c6a9fbda070b3224cd9b62b6c4c6bd6
Rahul Gupta <rahulgupta79777@gmail.com>
2026-09-08 01:03:38 +0530

    Time: 0 ms (100%), Space: 10 MB (97.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -17,14 +17,13 @@ public:
 
         //tabulation
         if(nums.size()==1) return nums[0];
-        if(nums.size()==2) return max(nums[0],nums[1]);
-        int a=nums[0];
-        int b=max(nums[0],nums[1]);
+        int a=0;
+        int b=0;
         int c=0;
-        for(int i=2;i<nums.size();i++){
-            c=max(b,nums[i]+a);
-            a=b;
-            b=c;
+        for(int i=0;i<nums.size();i++){
+            c = max(b, nums[i] + a);
+            a = b;
+            b = c;
         }
         return c;
     }
